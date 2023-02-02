edb9b1ce878f057c2459ecbc7b1374bad1cc7831
Lucjan Konopka <111157625+lucjankonopka@users.noreply.github.com>
2023-02-02 14:32:42 +0100

    Cleaning steps corrected

--- cyclistic_data_cleaning.py
+++ cyclistic_data_cleaning.py
@@ -37,7 +37,13 @@ df_complete = df_complete.reset_index(level=0)
 df_complete.dtypes
 # Columns "started_at" and "ended_at" should be in datetype format:
 df_complete['started_at'] = pd.to_datetime(df_complete['started_at'])
+# Checking first cell:
+df_complete['started_at'][0]
+
 df_complete['ended_at'] = pd.to_datetime(df_complete['ended_at'])
+# Checking first cell:
+df_complete['ended_at'][0]
+
 
 # Checking for "typos" or "misspellings" in the whole dataset:
 df_complete.rideable_type.unique()
@@ -48,11 +54,11 @@ df_complete.start_station_id.value_counts()
 df_complete.start_station_name.value_counts()
 df_complete.end_station_id.value_counts()
 df_complete.end_station_name.value_counts()
-# There are many names and id's that occurs just once. 
+# There are many names and id's that occur just once. 
 # They could be error inputs or new/deleted stations. To further investigation
 
 
-# Checking lenght of the id's
+# Checking length of the id's
 df_complete['ride_id'].map(len).unique()
 # All id's have 16 characters
 
@@ -105,22 +111,28 @@ no_nan_data.shape
 # Searching for the unwanted data - test data.
 # Context: TEST found in one of the start stations id's "Hubbard Bike-checking (LBS-WH-TEST)"
 no_nan_data['start_station_id'].str.contains('TEST').value_counts()
+# 1207 test rides found in start station id's. All should not be considered.
+
 no_nan_data['start_station_name'].str.contains('TEST').value_counts()
-# 1207 test rides found in start station id's. All should not be considered:
+# No test rides found in start station names.
 # Deleting test rides:
 no_test_start_data = no_nan_data[no_nan_data['start_station_id'].str.contains(
     'TEST') != True]
 
+
 no_test_start_data['end_station_id'].str.contains('TEST').value_counts()
+# 254 test rides found in rest end station id's. All should not be considered.
+
 no_test_start_data['end_station_name'].str.contains('TEST').value_counts()
-# 254 test rides found in rest end station id's. All should not be considered:
+# No test rides found in end station names.
+
 # Deleting test rides:
 no_nan_no_test_data = no_test_start_data[no_test_start_data['end_station_id'].str.contains(
     'TEST') != True]
 # 4472680 rows left
 
 # Dataframe with no test rides and no NaN values:
-no_nan_no_test_data.head()
+no_nan_no_test_data.head(5)
 
 # Creating copy of the dataframe to add new column:
 df_ridetime = no_nan_no_test_data.copy(deep=True)
@@ -131,34 +143,47 @@ df_ridetime.insert(
 # Changing ride time to seconds:
 df_ridetime['ride_time[s]'] = df_ridetime['ride_time[s]'].astype(
     'timedelta64[s]')
+df_ridetime.head(5)
+
 
 # Sorting data by ride time:
 df_rt_sorted = df_ridetime.sort_values(by='ride_time[s]')
+df_rt_sorted.head(5)
+
 
 # Checking for negative ride time values:
 df_rt_sorted[df_rt_sorted['ride_time[s]'] < 0]
 df_rt_sorted[df_rt_sorted['started_at'] > df_rt_sorted['ended_at']]
+
 # 71 wrong inputs with negative ride times. Deleting:
 df_rt_no_neg = df_rt_sorted[df_rt_sorted['ride_time[s]'] > 0]
+df_rt_no_neg.head(5)
+
+
 # Checking for the low ride time values, assuming these are incorrect or irrelevant records (below 60s):
 df_rt_no_neg[df_rt_no_neg['ride_time[s]'] < 60]
 # There are 73439 inputs with ride time below 60s. Deleting:
 df_rt_cleaned = df_rt_sorted[df_rt_sorted['ride_time[s]'] >= 60]
+df_rt_cleaned.head(5)
+
 
 # Data sorted by date:
 df_sort_date = df_rt_cleaned.sort_values(by="started_at")
+df_sort_date.head(5)
 
-# For the analysis purposes ride id's, station id's and coordinates are irrelevant.
-df_sort_date.columns
+
 columns_to_be_dropped = ['ride_id', 'start_station_id',
                          'end_station_id', 'start_lat', 'start_lng', 'end_lat', 'end_lng']
 df_drop_columns = df_sort_date.drop(columns=columns_to_be_dropped)
+df_drop_columns.head(5)
+
 
 ## Adding columns with the day of the week that each ride started
 # 0 - Monday, 6 - Sunday:
 df_drop_columns.insert(loc=4, column='weekday', value=df_ridetime['started_at'].dt.weekday)
 
 all_rides = df_drop_columns.copy(deep=True)
+all_rides.head(5)
 
 # weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
@@ -175,6 +200,8 @@ all_cleaned_rides_new_columns.columns = ['BikeType',
                                         'StartStation',
                                         'EndStation',
                                         'UserType']
+all_cleaned_rides_new_columns.head(5)
+
 
 # EXPORTING CLEAN DATA:
 all_cleaned_rides_new_columns.to_csv('cleaned_data/cyclistic_202110-202209_cleaned.csv')
